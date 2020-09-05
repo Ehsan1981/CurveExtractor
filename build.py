61a882b766b35f19d885f3b2d2d9e9f6689d0de7
BrunoB81HK <50412835+BrunoB81HK@users.noreply.github.com>
2020-09-05 15:10:11 -0400

    Added a build script

---
+++ build.py
@@ -0,0 +1,26 @@
+from zipfile import ZipFile, ZIP_DEFLATED
+import PyInstaller.__main__
+import shutil
+import os
+
+VER = "2.1"
+
+# Build the executable
+PyInstaller.__main__.run([
+    '--name=CurveFinder',
+    '--onefile',
+    '--noconsole',
+    '--icon=icon.ico',
+    'curvefinder.py'
+])
+
+# Create the distribution zip file
+with ZipFile('curvefinder_v{0:s}.zip'.format(VER), 'w', compression=ZIP_DEFLATED, compresslevel=9) as zipfile:
+    zipfile.write('dist/CurveFinder.exe', 'CurveFinder.exe')
+    zipfile.write('placeholder.png', 'data/placeholder.png')
+    zipfile.write('icon.png', 'data/icon.png')
+
+# Remove dist and build folder
+shutil.rmtree('dist/')
+shutil.rmtree('build/')
+os.remove('CurveFinder.spec')
