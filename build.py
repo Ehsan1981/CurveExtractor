b6581c588fb11f95c00cfc0fbcdf52145ebd2547
Bruno-Pier Busque <bruno-pier.busque@usherbrooke.ca>
2022-01-09 22:02:01 -0500

    Updated the build script

--- build.py
+++ build.py
@@ -14,41 +14,43 @@ parser.add_argument('-cl', '--clean', action="store_true", default=False, help="
 args = parser.parse_args()
 ####################################################
 
+if platform.system() == "Windows":
+    os_name = "win"
+    separator = ';'
+    splash = True
+elif platform.system() == "Linux":
+    os_name = "linux"
+    separator = ':'
+    splash = True
+elif platform.system() == "Darwin":
+    os_name = "macos"
+    separator = ':'
+    splash = False
+else:
+    sys.exit(0)
+####################################################
+
 # Build the executable
 if args.build:
     arguments = [
         'curvefinder.py',
         '--onefile',
         '--noconsole',
-        '--icon=resources/icon.ico'
+        '--icon=resources/icon.ico',
+        f'--name=CurveFinder_v{VER}_{os_name}',
+        f'--add-data=resources/icon.ico{separator}data',
+        f'--add-data=resources/placeholder.png{separator}data'
     ]
-
-    if platform.system() == "Windows":
-        os_name = "win"
-        separator = ';'
-        splash = True
-    elif platform.system() == "Linux":
-        os_name = "linux"
-        separator = ':'
-        splash = True
-    elif platform.system() == "Darwin":
-        os_name = "macos"
-        separator = ':'
-        splash = False
-    else:
-        sys.exit(0)
-
-    arguments.append(f'--name=CurveFinder_v{VER}_{os_name}')
-    arguments.append(f'--add-data=resources/icon.ico{separator}data')
-    arguments.append(f'--add-data=resources/placeholder.png{separator}data')
 
     if splash:
         arguments.append('--splash=resources/splash.png')
 
     PyInstaller.__main__.run(arguments)
+####################################################
 
 # Remove dist and build folder
 if args.clean:
     shutil.rmtree('dist/')
     shutil.rmtree('build/')
-    os.remove('CurveFinder.spec')
+    os.remove(f'CurveFinder_v{VER}_{os_name}.spec')
+####################################################
